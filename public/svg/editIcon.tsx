a88d37726b03b736364a8f8038ce6cb29adcf1d9
Developenzes <miroslav.penzes@gmail.com>
2024-07-19 22:16:42 +0200

    incons for crud created

---
+++ public/svg/editIcon.tsx
@@ -0,0 +1,46 @@
+import { IconProps } from "@/app/components/todos/types";
+
+const EditIcon = ({ width, height }: IconProps) => {
+  return (
+    <svg
+      width={width || 16}
+      height={height || 16}
+      viewBox="0 0 24 24"
+      xmlns="http://www.w3.org/2000/svg"
+      fill="currentcolor"
+    >
+      <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
+      <g
+        id="SVGRepo_tracerCarrier"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      ></g>
+      <g id="SVGRepo_iconCarrier">
+        <g id="Complete">
+          <g id="edit">
+            <g>
+              <path
+                d="M20,16v4a2,2,0,0,1-2,2H4a2,2,0,0,1-2-2V6A2,2,0,0,1,4,4H8"
+                fill="none"
+                stroke="currentcolor"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="2"
+              ></path>
+              <polygon
+                fill="none"
+                points="12.5 15.8 22 6.2 17.8 2 8.3 11.5 8 16 12.5 15.8"
+                stroke="currentcolor"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="2"
+              ></polygon>
+            </g>
+          </g>
+        </g>
+      </g>
+    </svg>
+  );
+};
+
+export default EditIcon;
