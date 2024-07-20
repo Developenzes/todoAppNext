cc65669dd1fa6a4068b589964b7f5bbe85b4909a
Developenzes <miroslav.penzes@gmail.com>
2024-07-20 13:50:52 +0200

    file for icons moved to components

--- src/app/components/icons/editIcon.tsx
+++ src/app/components/icons/editIcon.tsx
@@ -9,11 +9,11 @@ const EditIcon = ({ width, height }: IconProps) => {
       xmlns="http://www.w3.org/2000/svg"
       fill="currentcolor"
     >
-      <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
+      <g id="SVGRepo_bgCarrier" strokeWidth="0"></g>
       <g
         id="SVGRepo_tracerCarrier"
-        stroke-linecap="round"
-        stroke-linejoin="round"
+        strokeLinecap="round"
+        strokeLinejoin="round"
       ></g>
       <g id="SVGRepo_iconCarrier">
         <g id="Complete">
@@ -23,17 +23,17 @@ const EditIcon = ({ width, height }: IconProps) => {
                 d="M20,16v4a2,2,0,0,1-2,2H4a2,2,0,0,1-2-2V6A2,2,0,0,1,4,4H8"
                 fill="none"
                 stroke="currentcolor"
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                stroke-width="2"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
               ></path>
               <polygon
                 fill="none"
                 points="12.5 15.8 22 6.2 17.8 2 8.3 11.5 8 16 12.5 15.8"
                 stroke="currentcolor"
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                stroke-width="2"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
               ></polygon>
             </g>
           </g>
